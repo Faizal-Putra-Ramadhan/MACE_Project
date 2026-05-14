cb2a4661e688abb6a287b79636a3c6aa96e874af
Faizal putra Ramadhan <2300018199@webmail.uad.ac.id>
2026-05-14 20:04:49 +0700

    cleanup: remove debug logs

--- api/auth/login.js
+++ api/auth/login.js
@@ -5,11 +5,9 @@ import User from '../../models/User.js';
 import { withMiddleware } from '../../lib/auth.js';
 
 async function handler(req, res) {
-    if (req.method !== 'POST' && req.method !== 'GET') {
-        return res.status(405).json({ message: `Method ${req.method} not allowed` });
+    if (req.method !== 'POST') {
+        return res.status(405).json({ message: 'Method not allowed' });
     }
-
-    console.log(`[Login] Method: ${req.method}, Body:`, req.body);
 
     const { email, password } = req.body;
 
