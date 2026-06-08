e686aad58ae91d6b5002429da0f9b11562d69718
Faizal putra Ramadhan <2300018199@webmail.uad.ac.id>
2026-06-08 14:39:50 +0700

    add Data again

--- vite.config.js
+++ vite.config.js
@@ -10,4 +10,16 @@ export default defineConfig({
     tailwindcss(),
     babel({ presets: [reactCompilerPreset()] })
   ],
+  build: {
+    rolldownOptions: {
+      output: {
+        // Fungsi ini akan memisahkan library besar dari node_modules menjadi file tersendiri
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        },
+      },
+    },
+  },
 })
